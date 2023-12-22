1bc3cefdd43b4d24d5ae9bd4c751dfa2a586cf45
haikallf <haikallzrd@gmail.com>
2023-12-22 10:14:23 +0700

    init case 4

---
+++ SDD-Case-4/SDD-Case-4/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SDD-Case-4
+//
+//  Created by Haikal Lazuardi on 22/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
