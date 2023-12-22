32ec73b960c4c0deb8f1814b34843b64cb64519f
haikallf <haikallzrd@gmail.com>
2023-12-22 12:05:00 +0700

    firebase setup, working push notif, send notif while on background

--- SDD-Case-4/SDD-Case-4/AppDelegate.swift
+++ SDD-Case-4/SDD-Case-4/AppDelegate.swift
@@ -6,31 +6,131 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseMessaging
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    let gcmMessageIDKey: String = "gcm.Message_ID"
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        UNUserNotificationCenter.current().delegate = self
+        
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: { _, _ in }
+        )
+        
+        application.registerForRemoteNotifications()
+        
+        Messaging.messaging().delegate = self
+        
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+}
 
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    // Receive displayed notifications for iOS 10 devices.
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification) async
+    -> UNNotificationPresentationOptions {
+        let userInfo = notification.request.content.userInfo
+        
+        // With swizzling disabled you must let Messaging know about the message, for Analytics
+        // Messaging.messaging().appDidReceiveMessage(userInfo)
+        
+        // ...
+        
+        // Print full message.
+        print(userInfo)
+        
+        handleNotification(userInfo: userInfo)
+        
+        // Change this to your preferred presentation option
+        return [[.alert, .sound]]
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse) async {
+        let userInfo = response.notification.request.content.userInfo
+        
+        // ...
+        
+        // With swizzling disabled you must let Messaging know about the message, for Analytics
+        // Messaging.messaging().appDidReceiveMessage(userInfo)
+        
+        // Print full message.
+        print(userInfo)
+    }
+    
+    func handleNotification(userInfo: [AnyHashable: Any]) {
+        print("Received notification: \(userInfo)")
+        
+        if let deepLinkString = userInfo["deepLink"] as? String, let deepLinkURL = URL(string: deepLinkString) {
+            // Handle the deep link URL (e.g., navigate to a specific screen)
+            print("Deep Link: \(deepLinkURL)")
+            
+            // Add your custom logic here based on the deep link
+        }
+    }
+    
+    func application(_ application: UIApplication,
+                     didReceiveRemoteNotification userInfo: [AnyHashable: Any]) async
+    -> UIBackgroundFetchResult {
+        // If you are receiving a notification message while your app is in the background,
+        // this callback will not be fired till the user taps on the notification launching the application.
+        // TODO: Handle data of notification
+        
+        // With swizzling disabled you must let Messaging know about the message, for Analytics
+        // Messaging.messaging().appDidReceiveMessage(userInfo)
+        
+        // Print message ID.
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID: \(messageID)")
+        }
+        
+        // Print full message.
+        print(userInfo)
+        
+        return UIBackgroundFetchResult.newData
+    }
+    
+}
+
+extension AppDelegate: MessagingDelegate {
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        if let token = fcmToken {
+            UserDefaults.standard.set(token, forKey: "fcmToken")
+        }
+        
+        print("Firebase registration token: \(fcmToken)")
+        
+        let dataDict: [String: String] = ["token": fcmToken ?? ""]
+        NotificationCenter.default.post(
+            name: Notification.Name("FCMToken"),
+            object: nil,
+            userInfo: dataDict
+        )
+        // TODO: If necessary send token to application server.
+        // Note: This callback is fired at each app startup and whenever a new token is generated.
+    }
 }
 
--- SDD-Case-4/SDD-Case-4/ViewController.swift
+++ SDD-Case-4/SDD-Case-4/ViewController.swift
@@ -12,6 +12,56 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(appMovedToBackground), name: UIApplication.willResignActiveNotification, object: nil)
+    }
+    
+    @objc 
+    func appMovedToBackground() {
+        print("App moved to background!")
+        
+        guard let token = UserDefaults.standard.string(forKey: "fcmToken") else { return }
+        
+        let payload: [String: Any] = [
+            "to": token,
+            "data": [
+                "deepLink": "sample.id://transfer/result?title=SUKSES&transactionCode=RF001-204"
+            ],
+            "notification": [
+                "title": "Push Notification!",
+                "body": "Please check your transaction result"
+            ]
+        ]
+        
+        // Convert payload to Data
+        if let data = try? JSONSerialization.data(withJSONObject: payload, options: []) {
+            // Send the payload to Firebase
+            sendPayloadToFirebase(data: data)
+        }
+    }
+    
+    func sendPayloadToFirebase(data: Data) {
+        // Use your server or a cloud function to send the payload to Firebase
+        // You can use a networking library or URLSession to make a POST request to the FCM endpoint
+        // Replace "<Your FCM Server Key>" with your actual FCM Server Key
+        let fcmUrl = URL(string: "https://fcm.googleapis.com/fcm/send")!
+        var request = URLRequest(url: fcmUrl)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("key=AAAAR-N_4Wg:APA91bFS81RpYNvgwn3cjCz3qRmH6yECe5vxp87_KAxKIXhRJlCoQ3WwK5Qa5rksutHk4k5uiYJjAI6EWLFbhGBTVc-0xeCyse_SOJmbLGVRFTZa3c1ZvW6hEcuGbF_t3hSW85Y4SweD", forHTTPHeaderField: "Authorization")
+        request.httpBody = data
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("Error sending payload to Firebase: \(error)")
+            } else if let data = data {
+                let responseString = String(data: data, encoding: .utf8)
+                print("Response from Firebase: \(responseString ?? "")")
+            }
+        }
+        
+        task.resume()
     }
 }
 
